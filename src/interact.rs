1e43925551adedee4e5065ff967faca3b6f1bde9
Shaun Keys <mariobuddy@gmail.com>
2023-07-20 23:06:48 -0600

    Switch to tabs, format project, and make files required

--- src/interact.rs
+++ src/interact.rs
@@ -1,4 +1,6 @@
 /// This function allows concurrent async threads to share the console.
-pub async fn interact<T: Send + Sync + 'static>(func: impl FnOnce() -> T + Send + Sync + 'static) -> T {
+pub async fn interact<T: Send + Sync + 'static>(
+	func: impl FnOnce() -> T + Send + Sync + 'static,
+) -> T {
 	return tokio::task::spawn_blocking(|| func()).await.unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ mod interact;
 
 use anyhow::anyhow;
 use clap::{Parser, Subcommand};
+use get_st_track::get_comparison_track;
 use lazy_static::lazy_static;
 use std::{path::PathBuf, process::Command};
-use get_st_track::get_comparison_track;
 
 lazy_static! {
-	static ref THEME: dialoguer::theme::ColorfulTheme = {
-		dialoguer::theme::ColorfulTheme::default()
-	};
+	static ref THEME: dialoguer::theme::ColorfulTheme =
+		dialoguer::theme::ColorfulTheme::default();
 }
 
 #[derive(Parser)]
@@ -22,13 +21,14 @@ struct Cli {
 #[derive(Subcommand)]
 enum AutotaggerCommand {
 	ExtractSubtitles {
-	    files: Vec<PathBuf>,
-	}
+		#[arg(required = true)]
+		files: Vec<PathBuf>,
+	},
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let args = Cli::parse();
+	let args = Cli::parse();
 
 	match args.command {
 		AutotaggerCommand::ExtractSubtitles { files } => {
@@ -37,9 +37,15 @@ async fn main() -> anyhow::Result<()> {
 				let extract_result = Command::new("mkvextract")
 					.arg("tracks")
 					.arg(&file)
-					.arg(format!("{}:{}", st_track.number - 1, file.with_extension("").to_str().unwrap()))
-					.spawn().unwrap()
-					.wait().unwrap();
+					.arg(format!(
+						"{}:{}",
+						st_track.number - 1,
+						file.with_extension("").to_str().unwrap()
+					))
+					.spawn()
+					.unwrap()
+					.wait()
+					.unwrap();
 				if !extract_result.success() {
 					return Err(anyhow!("Failed to extract subtitles"));
 				}
@@ -47,5 +53,5 @@ async fn main() -> anyhow::Result<()> {
 		}
 	}
 
-    return Ok(());
+	return Ok(());
 }
